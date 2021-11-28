837e198a6892c31c0ea84c23761c54b1a8da3c55
KevinDanae <kevin.danaea@gmail.com>
2021-11-28 16:59:02 -0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Kevin Aguilera on 28/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
